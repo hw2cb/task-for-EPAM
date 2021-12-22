652b39a46bf8961010a4bc6c447b4d2de4ea5f66
hw2cb <dim5.dim5.960@mail.ru>
2021-12-22 21:29:03 +0400

    completed Task 1.1.4 in block 1.1

---
+++ Task 1/Task_1_1/Tasks1_1/Task1_1_4/Program.cs
@@ -0,0 +1,56 @@
+﻿/* TASK 1.1.4
+ * X-MAX TREE
+ * Написать программу, которая запрашивает с клавиатуры число N и
+ * выводит на экран следующее изображение состоящее из N треугольников:
+ *                  *
+ *                  *
+ *                 ***
+ *                  *
+ *                 ***
+ *                *****
+ *                  *
+ *                 ***
+ *                *****
+ *               *******
+ *                  *
+ *                 ***
+ *                *****
+ *               *******
+ *              ********* 
+ * 
+ */
+
+
+using SD;
+
+namespace Tasks1_1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int numberN = SD.SD.EnterNumber();
+            for (int i = numberN; i >= 0; i--)
+            {
+                int countStar = 1;
+                int countSpace = numberN;
+                for (int j = 0; j <= numberN-i; j++)
+                {
+                    //расставляем пробелы
+                    for (int k = 0; k < countSpace; k++)
+                    {
+                        Console.Write(' ');
+                    }
+                    //звезды
+                    for(int k = 0; k < countStar; k++)
+                    {
+                        Console.Write('*');
+                    }
+                    countStar+=2;
+                    countSpace--;
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
