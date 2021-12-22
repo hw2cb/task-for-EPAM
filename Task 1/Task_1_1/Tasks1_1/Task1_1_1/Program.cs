26c76462b91f2e20c45fde9378fe908819bf163c
hw2cb <dim5.dim5.960@mail.ru>
2021-12-22 19:01:50 +0400

    completed Task 1.1.1 in block 1.1, start preparing 1.1.2

---
+++ Task 1/Task_1_1/Tasks1_1/Task1_1_1/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Task 1.1.1
+ * RECTANGLE
+ * Написать программу, которая определяетс площадь прямоугольника со сторонами a и b.
+ * Если пользователь вводит некорректные значения (отрицательные или ноль), должно выдаваться сообщение об ошибке.
+ * Возможность ввода пользователем строки "абвгд" или нецелых чисел игнорировать.
+ */
+
+namespace Tasks1_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Цикл while обеспечит повторный ввод при некорректных значениях
+            while(!Enter())
+            {
+                Console.WriteLine("Некорректный ввод!");
+            }
+        }
+        private static bool Enter()
+        {
+            //метод ввода чисел в программу
+            Console.WriteLine("Введите сторону a: ");
+            int.TryParse(Console.ReadLine(), out int a);
+            Console.WriteLine("Введите сторону b: ");
+            int.TryParse(Console.ReadLine(), out int b);
+
+            if (a <= 0 || b <= 0) return false;
+
+            GetResult(a, b);
+            return true;
+        }
+        private static void GetResult(int a, int b)
+        {
+            //вспомогательный метод вывода на консоль результата
+            Console.WriteLine($"Площаль прямоугольника со сторонами a:{a} b:{b} = {a * b}");
+        }
+    }
+}
