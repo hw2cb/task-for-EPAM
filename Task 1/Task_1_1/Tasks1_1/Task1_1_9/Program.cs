cfd17e24266285c7b6e0f7086d1553ceebdbfcf6
hw2cb <dim5.dim5.960@mail.ru>
2022-01-04 00:33:46 +0400

    completed Task 1.1.9 in block 1.1

---
+++ Task 1/Task_1_1/Tasks1_1/Task1_1_9/Program.cs
@@ -0,0 +1,39 @@
+﻿/*TASK 1.1.9
+ * NON-NEGATIVE SUM
+ * Написать программу, которая определяет сумму неотрицательных элементов в одномерном массиве. 
+ * Число элементов в массиве и их тип определяются разработчиком.
+ * 
+ */
+
+namespace Tasks1_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] array = FillArray(10);
+            Console.WriteLine(String.Join(", ", array));
+            Console.WriteLine($"Сумма неотрицательных элементов: {NegativeSum(array)}");
+        }
+        public static int NegativeSum(int[] array)
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if(array[i]>0)
+                    sum+=array[i];
+            }
+            return sum;
+        }
+        public static int[] FillArray(int size)
+        {
+            Random r = new Random();
+            int[] res = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                res[i] = r.Next(-7, 7);
+            }
+            return res;
+        }
+    }
+}
