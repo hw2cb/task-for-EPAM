3ee5e97dc0005f46df8501f831db22c4e4f2ee61
hw2cb <dim5.dim5.960@mail.ru>
2022-01-14 21:48:11 +0400

    completed 2.1.1

---
+++ Task 2/Task2_1_1/Task2_1_1/Program.cs
@@ -0,0 +1,94 @@
+﻿using Task2_1_1;
+
+namespace Tasks2_2_1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Индексатор
+            Console.WriteLine("Демонстрация индексатора:");
+            StringCreator str = new StringCreator('H', 'e', 'l', 'l', 'o', '!');
+            Console.WriteLine($"Исходная строка: {str} \nВывожу на консоль элемент с индексом 2");
+            Console.WriteLine(str[2]);
+
+            Console.WriteLine();
+
+            //Конкатенация
+            Console.WriteLine("Демонстрация конкатенации:");
+            StringCreator str2 = new StringCreator(' ', 'w', 'o', 'r', 'l', 'd', '!');
+            Console.WriteLine($"Первая строка: {str}\nВторая строка: {str2}");
+            Console.WriteLine($"Результат: {str + str2}");
+
+            Console.WriteLine();
+
+            //Substring
+            Console.WriteLine("Демонстрация Substring:");
+            Console.WriteLine($"Исходная строка: {str2} беру подстроку с начальным индексом 1, с длиной 3");
+            StringCreator substrings = str2.Substring(1, 3);
+            Console.WriteLine($"Результат: {substrings}");
+
+            Console.WriteLine();
+
+            //Split
+            Console.WriteLine("Демонстрация Split по ! знаку:");
+            StringCreator str3 = new StringCreator(' ', 'i', 't', 's', 'm', 'y');
+            StringCreator str4 = str + str2 + str3;
+            Console.WriteLine($"Исходная строка: {str4}");
+            char[] separators = new char[] { '!' };
+            StringCreator[] strs = StringCreator.MySplit(str4, separators);
+            Console.WriteLine("Результат сплита:");
+            for (int i = 0; i < strs.Length; i++)
+            {
+                Console.WriteLine(strs[i]);
+            }
+
+            Console.WriteLine();
+
+            //Поиск символов
+            Console.WriteLine($"Исходная строка: {str}");
+            Console.WriteLine("Демонстрация поиска символа l и возврат его индекса:");
+            Console.WriteLine(str.FindCharIndex('l'));
+            Console.WriteLine("Демонстрация поиска символа h и возврат bool:");
+            Console.WriteLine(str.FindChar('h'));
+
+            Console.WriteLine();
+
+            //Приведение к массиву
+            Console.WriteLine("Демонстрация приведения к массиву:");
+            char[] array = str.ToCharArray();
+            Console.WriteLine(string.Join(',', array));
+
+            Console.WriteLine();
+
+            //Trim
+            Console.WriteLine("Демонстрация Trim:");
+            Console.WriteLine(str2);
+            StringCreator trimming = str2.Trim();
+            Console.WriteLine(trimming);
+
+            Console.WriteLine();
+
+            //Equals, ==, !=
+            Console.WriteLine("Демонстрация сравнения:");
+            StringCreator first = new StringCreator('h', 'i');
+            StringCreator second = new StringCreator('h', 'i');
+            StringCreator third = new StringCreator('f', 'g', 'd');
+            Console.WriteLine($"hi == hi - {first==second}");
+            Console.WriteLine($"hi != hi - {first != second}");
+            Console.WriteLine($"hi == fgd - {first == third}");
+            Console.WriteLine($"hi != fgd - {first!= third}");
+            Console.WriteLine($"hi eqauls second - {first.Equals(second)}");
+
+            Console.WriteLine();
+
+            //Поиск цифр в строке
+            Console.WriteLine("Демонстрация поиска цифр в строке:");
+            StringCreator digitStr = new StringCreator('H', '2', 'e', 'l', 'l', 'o', '!', '5', '2', '9');
+            Console.WriteLine($"Исходная строка: {digitStr}");
+            int[] digitFromStr = digitStr.GetDegit();
+            Console.WriteLine(string.Join(',', digitFromStr));
+
+        }
+    }
+}
---
+++ Task 2/Task2_1_1/Task2_1_1/StringCreator.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_1_1
+{
+    /* Реализовано:
+     * 
+     * Индексатор
+     * Конкатенация
+     * Substring
+     * Split (по массиву разделителей)
+     * Поиск символа (возвращает индекс)
+     * Поиск символа (возвращает bool)
+     * Метод приведения к массиву
+     * Trim (обрезка пробелов)
+     * Переопределен Equals
+     * Операторы сравнения == / !=
+     * Поиск цифр в строке
+     */
+    public class StringCreator
+    {
+        private char[] _chars;
+        public int Capacity { get; private set; }
+        public StringCreator()
+        {
+            _chars = new char[4];
+            Capacity = 4;
+        }
+        public StringCreator(params char [] str)
+        {
+            _chars = new char[str.Length];
+            Capacity = str.Length;
+            _chars = str;
+        }
+        public StringCreator(int capacity)
+        {
+            Capacity = capacity;
+            _chars = new char[capacity];
+        }
+        public StringCreator(StringCreator a, StringCreator b)
+        {
+            if (a == null || b == null) throw new ArgumentNullException();
+
+            Capacity = a.Capacity + b.Capacity;
+            _chars = AppendsChar(a, b, Capacity);
+        }
+        public char this[int index]
+        {
+            get
+            {
+                if(index<0 || index > Capacity) throw new ArgumentOutOfRangeException();
+                return _chars[index];
+            }
+            set
+            {
+                if (index < 0 || index > Capacity) throw new ArgumentOutOfRangeException();
+                _chars[index] = value;
+            }
+        }
+        public static StringCreator operator + (StringCreator a, StringCreator b)
+        {
+            return new StringCreator(a, b);
+        }
+        public static bool operator == (StringCreator a, StringCreator b)
+        {
+            return a.Equals(b);
+        }
+        public static bool operator != (StringCreator a, StringCreator b)
+        {
+            return !a.Equals(b);
+        }
+        public char[] ToCharArray()
+        {
+            char[] chars = new char[Capacity];
+            for (int i = 0; i < Capacity; i++)
+            {
+                chars[i] = _chars[i];
+            }
+            return chars;
+        }
+        public int FindCharIndex(char symbol)
+        {
+            /* Метод находит индекс первого вхождения символа в строке
+             */
+            for(int i = 0; i < Capacity; i++)
+            {
+                if (_chars[i] == symbol) return i;
+            }
+            return -1;
+        }
+        public bool FindChar(char symbol)
+        {
+            if(FindCharIndex(symbol)==-1) return false;
+            return true;
+        }
+
+        public static StringCreator[] MySplit(StringCreator str, char[] separators)
+        {
+            //добавил метод разделения строки с сохранением разделителя
+            int startIndex = 0;
+            //использую хэшсет дабы защититься от попытки сунуть в метод повторяющиеся разделители
+            HashSet<char> sep = new HashSet<char>(separators);
+            List<StringCreator> res = new List<StringCreator>();
+
+            for (int i = 0; i < str.Capacity; i++)
+            {
+                if (sep.Contains(str[i]))
+                {
+                    res.Add(str.Substring(startIndex, (i - startIndex) +1).Trim());
+                    
+                    startIndex = i + 1;
+                }
+            }
+            //если вдруг последняя строчка без знака
+            if(startIndex != str.Capacity)
+            {
+                res.Add(str.Substring(startIndex, (str.Capacity - startIndex)).Trim());
+            }
+            return res.ToArray();
+        }
+        public StringCreator Substring(int startIndex, int lenght)
+        {
+            if ((startIndex + lenght) > Capacity) throw new ArgumentOutOfRangeException();
+
+            char[] chars = new char[lenght];
+            for (int i = startIndex, j=0; j < lenght; i++, j++)
+            {
+                chars[j] = _chars[i];
+            }
+            StringCreator str = new StringCreator(chars);
+            return str;
+        }
+        public StringCreator Trim()
+        {
+            //метод обрезки пробелов из начала и конца строки
+            if(_chars[0] == ' ')
+            {
+                //обрезаю пробел в начале
+                char[] newChars = _chars[1..];
+                return new StringCreator(newChars);
+            }
+            if(_chars[_chars.Length - 1] == ' ')
+            {
+                //обрезаю пробел в конце
+                char[] newChars = _chars[..(_chars.Length-1)];
+                return new StringCreator(newChars);
+            }
+            if(_chars[0] == ' ' && _chars[_chars.Length - 1] == ' ')
+            {
+                //обрезаю и в начале и в конце
+                char[] newChars = _chars[1..(_chars.Length-1)];
+                return new StringCreator(newChars);
+            }
+            return new StringCreator(_chars);
+        }
+        public int[] GetDegit()
+        {
+            List<int> digit = new List<int>();
+            for(int i=0; i<_chars.Length; i++)
+            {
+                if(char.IsDigit(_chars[i]))
+                {
+                    digit.Add(_chars[i] - '0');
+                }
+            }
+            return digit.ToArray();
+        }
+
+        private static char[] AppendsChar(StringCreator a, StringCreator b, int capacity)
+        {
+            char[] buff = new char[capacity];
+            int j = 0;
+            for (int i = 0; i < a.Capacity; i++, j++)
+            {
+                buff[j] = a[i];
+            }
+            for (int i = 0; i < b.Capacity; i++, j++)
+            {
+                buff[j] = b[i];
+            }
+            return buff;
+        }
+        public override string ToString()
+        {
+            return new string(_chars);
+        }
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) return false;
+            if(this.GetType() != obj.GetType()) return false;
+
+            //сравниваю поля
+            //считаю что s не будет null, так как перед этим сравнивал типы
+            StringCreator s = obj as StringCreator;
+            if(s.Capacity != Capacity) return false;
+
+            for(int i=0; i < Capacity; i++)
+            {
+                if(s[i] != _chars[i]) return false;
+            }
+            return true;
+        }
+    }
+}
