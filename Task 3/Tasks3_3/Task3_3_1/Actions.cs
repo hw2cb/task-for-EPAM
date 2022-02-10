550eb637e05ceb1bd3ec49de7a6ec779a80de348
hw2cb <dim5.dim5.960@mail.ru>
2022-02-11 00:02:55 +0400

    completed task 3.3.1

---
+++ Task 3/Tasks3_3/Task3_3_1/Actions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_3_1
+{
+    internal static class Actions
+    {
+        //сами методы
+        public static int Multiply(int element) => element * 2;
+        public static int PowTwo(int element) => element * element;
+
+        public static int Divide(int element)
+        {
+            //думаю не суть важно, пусть при нуле будет возвращаться 0
+            if (element == 0) return 0;
+            return element / 2;
+        }
+    }
+}
--- Task 3/Tasks3_3/Task3_3_1/Program.cs
+++ Task 3/Tasks3_3/Task3_3_1/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Tasks3_3
+﻿using Task3_3_1;
+
+namespace Tasks3_3
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
-            
+            Startup.Run();
         }
     }
 }
